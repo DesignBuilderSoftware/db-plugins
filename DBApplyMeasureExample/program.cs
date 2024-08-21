b2a83784fe3f454d26b25f9a07aa4fc9099248bf
voightp <vojtechpanek@seznam.cz>
2024-08-21 14:02:52 +0200

    init new plugin examples

---
+++ DBApplyMeasureExample/program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using System.ComponentModel.Composition;
+
+using DB.Extensibility.Contracts;
+using DB.Api;
+
+namespace DBApplyMeasureExample
+{
+    [Export(typeof(IPlugin2))]
+    public class ExamplePlugin : PluginBase2, IPlugin2
+    {
+        class MenuKeys
+        {
+            public const string Root = "root";
+            public const string Ecm1 = "ecm1";
+            public const string Ecm2 = "ecm2";
+        }
+        class MenuItem
+        {
+            public Action Action { get; set; }
+            public bool IsEnabled { get; set; }
+            public bool IsVisible { get; set; }
+            public MenuItem(
+                Action action = null,
+                bool enabled = true,
+                bool visible = true)
+            {
+                Action = action ?? delegate { };
+                IsEnabled = enabled;
+                IsVisible = visible;
+            }
+        }
+        private readonly Dictionary<string, MenuItem> _mMenuItems = new Dictionary<string, MenuItem>();
+        public override bool HasMenu
+        {
+            get { return true; }
+        }
+        public override string MenuLayout
+        {
+            get
+            {
+                StringBuilder menu = new StringBuilder();
+                menu.AppendFormat("*Apply ECM,{0}", MenuKeys.Root);
+                menu.AppendFormat("*>ECM 1 (Economiser),{0}", MenuKeys.Ecm1);
+                menu.AppendFormat("*>ECM 2 (Infiltration),{0}", MenuKeys.Ecm2);
+                return menu.ToString();
+            }
+        }
+        public override bool IsMenuItemVisible(string key)
+        {
+            return _mMenuItems[key].IsVisible;
+        }
+        public override bool IsMenuItemEnabled(string key)
+        {
+            return _mMenuItems[key].IsEnabled;
+        }
+        public override void OnMenuItemPressed(string key)
+        {
+            _mMenuItems[key].Action();
+        }
+        public override void Create()
+        {
+            _mMenuItems.Add(MenuKeys.Root, new MenuItem());
+            _mMenuItems.Add(MenuKeys.Ecm1, new MenuItem(SetEconomizer));
+            _mMenuItems.Add(MenuKeys.Ecm2, new MenuItem(SetInfiltration));
+        }
+
+        public Building GetCurrentBuilding()
+        {
+            Site site = ApiEnvironment.Site;
+            int buildingIndex = ApiEnvironment.CurrentBuildingIndex;
+            if (buildingIndex != -1)
+            {
+                return site.Buildings[buildingIndex];
+            }
+            return null;
+
+        }
+
+        public void SetEconomizer()
+        {
+            Building building = GetCurrentBuilding();
+            if (building != null)
+            {
+                const string economiserAttribute = "EconomiserControlType";
+                const string economiserType = "2-Fixed Drybulb";
+
+                List<string> airLoopNames = new List<string>();
+
+                var airHandlingUnits = building.HvacNetwork.Loops
+                    .Where(loop => loop.LoopType == HvacLoopType.Air)
+                    .SelectMany(loop => loop.SupplySubLoop.Components)
+                    .Where(component => component.ComponentType == HvacComponentType.AirHandlingUnit);
+                foreach (var unit in airHandlingUnits)
+                {
+                    airLoopNames.Add(unit.GetAttribute("Title"));
+                    unit.SetAttribute(economiserAttribute, economiserType);
+                }
+
+                MessageBox.Show(String.Format("Building {0}: set attribute '{1}' to '{2}' for loops [ {3} ]",
+                    building.GetAttribute("Title"), economiserAttribute,
+                    economiserType, String.Join(",", airLoopNames)));
+            }
+        }
+
+        public void SetInfiltration()
+        {
+            Building building = GetCurrentBuilding();
+            if (building != null)
+            {
+                const string infiltrationAttribute = "InfiltrationValue";
+                const string infiltrationValue = "0.15";
+
+                building.SetAttribute(infiltrationAttribute, infiltrationValue);
+
+                MessageBox.Show(String.Format("Building {0}: set attribute '{1}' to '{2}'.",
+                    building.GetAttribute("Title"), infiltrationAttribute, infiltrationValue));
+            }
+        }
+
+        public override void ModelLoaded()
+        {
+            _mMenuItems[MenuKeys.Root].IsEnabled = true;
+        }
+        public override void ModelUnloaded()
+        {
+            _mMenuItems[MenuKeys.Root].IsEnabled = false;
+        }
+    }
+}
---
+++ DBFDWRReport/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel.Composition;
+
+using DB.Extensibility.Contracts;
+using DB.Api;
+
+namespace DBApplyMeasureExample
+{
+    [Export(typeof(IPlugin2))]
+    public class ExamplePlugin : PluginBase2, IPlugin2
+    {
+        class MenuKeys
+        {
+            public const string Root = "root";
+            public const string ReportFDWR = "reportFDWR";
+        }
+        class MenuItem
+        {
+            public Action Action { get; set; }
+            public bool IsEnabled { get; set; }
+            public bool IsVisible { get; set; }
+            public MenuItem(
+                Action action = null,
+                bool enabled = true,
+                bool visible = true)
+            {
+                Action = action ?? delegate { };
+                IsEnabled = enabled;
+                IsVisible = visible;
+            }
+        }
+        private readonly Dictionary<string, MenuItem> mMenuItems = new Dictionary<string, MenuItem>();
+        public override bool HasMenu
+        {
+            get { return true; }
+        }
+        public override string MenuLayout
+        {
+            get
+            {
+                StringBuilder menu = new StringBuilder();
+                menu.AppendFormat("*Plugins,{0}", MenuKeys.Root);
+                menu.AppendFormat("*>Report FDWR,{0}", MenuKeys.ReportFDWR);
+                return menu.ToString();
+            }
+        }
+        public override bool IsMenuItemVisible(string key)
+        {
+            return mMenuItems[key].IsVisible;
+        }
+        public override bool IsMenuItemEnabled(string key)
+        {
+            return mMenuItems[key].IsEnabled;
+        }
+        public override void OnMenuItemPressed(string key)
+        {
+            mMenuItems[key].Action();
+        }
+        public override void Create()
+        {
+            mMenuItems.Add(MenuKeys.Root, new MenuItem());
+            mMenuItems.Add(MenuKeys.ReportFDWR, new MenuItem(ReportFdwr, false));
+        }
+
+        public void ReportFdwr()
+        {
+            Site site = ApiEnvironment.Site;
+            foreach (Building building in site.Buildings)
+            {
+                BuildingFdwr fdwr = CalculateFdwr(building);
+
+                StringBuilder report = new StringBuilder();
+                report.AppendFormat("Building: {0}\n", building.GetAttribute("Title"));
+                report.AppendFormat("    - Wall Area: {0:0.00}m2\n", fdwr.WallArea);
+                report.AppendFormat("    - Door Area: {0:0.00}m2\n", fdwr.DoorArea);
+                report.AppendFormat("    - Window Area: {0:0.00}m2\n", fdwr.WindowArea);
+                report.AppendFormat("    - FDWR: {0:0.00}%", fdwr.GetValue());
+
+                MessageBox.Show(report.ToString());
+            }
+        }
+        public class BuildingFdwr
+        {
+            public double WallArea { get; set; }
+            public double DoorArea { get; set; }
+            public double WindowArea { get; set; }
+
+            public BuildingFdwr() { }
+
+            public BuildingFdwr(double wallArea, double doorArea, double windowArea)
+            {
+                WallArea = wallArea;
+                DoorArea = doorArea;
+                WindowArea = windowArea;
+            }
+
+            public double GetValue()
+            {
+                return (WindowArea + DoorArea) / WallArea * 100;
+            }
+        }
+
+        public BuildingFdwr CalculateFdwr(Building building)
+        {
+            double totalWallArea = 0;
+            double totalDoorArea = 0;
+            double totalWindowArea = 0;
+
+            foreach (BuildingBlock block in building.BuildingBlocks)
+            {
+                foreach (Zone zone in block.Zones)
+                {
+                    foreach (Surface surface in zone.Surfaces)
+                    {
+                        if (surface.Type == SurfaceType.Wall)
+                        {
+                            foreach (Adjacency adjacency in surface.Adjacencies)
+                            {
+                                if (adjacency.IsExternal && adjacency.AdjacencyCondition == AdjacencyCondition.NotAdjacentToGround)
+                                {
+                                    totalWallArea += adjacency.Area;
+                                    foreach (Opening opening in adjacency.Openings)
+                                    {
+                                        if (opening.Type == OpeningType.Window)
+                                        {
+                                            totalWindowArea += opening.Area;
+                                        }
+                                        if (opening.Type == OpeningType.Door)
+                                        {
+                                            totalDoorArea += opening.Area;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return new BuildingFdwr(totalWallArea, totalDoorArea, totalWindowArea);
+        }
+        public override void ModelLoaded()
+        {
+            mMenuItems[MenuKeys.ReportFDWR].IsEnabled = true;
+        }
+        public override void ModelUnloaded()
+        {
+            mMenuItems[MenuKeys.ReportFDWR].IsEnabled = false;
+        }
+    }
+}
