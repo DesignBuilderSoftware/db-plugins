a6cd6bb1225a139487928f3f4ed6779ebad3c8a3
voightp <vojtechpanek@seznam.cz>
2025-03-13 14:05:32 +0100

    improve report apeparance and briging titles

--- DBReportBridgingLengths/Program.cs
+++ DBReportBridgingLengths/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.Composition;
 using System.IO;
+using System.Diagnostics;
 using System.Windows.Forms;
 using DB.Extensibility.Contracts;
 using DB.Api;
@@ -17,7 +18,7 @@ namespace DBReportBridgingLengths
         class MenuKeys
         {
             public const string Root = "root";
-            public const string ReportBridgingLengths = "report bridging lengths";
+            public const string ReportBridgingLengths = "report bridging";
         }
 
         class MenuItem
@@ -49,8 +50,8 @@ namespace DBReportBridgingLengths
             get
             {
                 StringBuilder menu = new StringBuilder();
-                menu.AppendFormat("*Plugins,{0}", MenuKeys.Root);
-                menu.AppendFormat("*>Report bridging lengths,{0}", MenuKeys.ReportBridgingLengths);
+                menu.AppendFormat("*Bridging,{0}", MenuKeys.Root);
+                menu.AppendFormat("*>Report bridging,{0}", MenuKeys.ReportBridgingLengths);
                 return menu.ToString();
             }
         }
@@ -89,8 +90,12 @@ namespace DBReportBridgingLengths
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                string fileName = saveFileDialog.FileName;
-                File.WriteAllText(fileName, report);
+                File.WriteAllText(saveFileDialog.FileName, report);
+                if (MessageBox.Show(@"Report generated successfully. Would you like to open it?", @"Report Complete",
+                        MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Process.Start(saveFileDialog.FileName);
+                }
             }
         }
 
@@ -104,13 +109,17 @@ namespace DBReportBridgingLengths
             report.AppendLine("Linear bridging report");
             report.AppendLine("----------------------");
             report.AppendLine();
-            report.AppendFormat("Building {0}{1}", building.GetAttribute("Title"), Environment.NewLine);
+            report.AppendFormat("Building: {0}{1}", building.GetAttribute("Title"), Environment.NewLine);
+            report.AppendLine();
+
             foreach (BuildingBlock block in building.BuildingBlocks)
             {
                 foreach (Zone zone in block.Zones)
                 {
-                    ZoneBridging bridging = new ZoneBridging(zone);
-                    report.Append(bridging.GetReport());
+                    if (zone.IsChildZone) continue;
+                    string title = block.GetAttribute("Title") + ":" + zone.GetAttribute("Title");
+                    ZoneBridging bridging = new ZoneBridging(title, zone);
+                    report.Append(bridging.GetReport(includeZeroLength: false));
                     report.AppendLine();
                 }
             }
@@ -122,60 +131,115 @@ namespace DBReportBridgingLengths
         public class ZoneBridging
         {
             private string zoneName;
-            private Dictionary<string, double> metalCladBridging;
-            private Dictionary<string, double> noCladBridging;
+            private Dictionary<string, double> metalCladBridgingLengths;
+            private Dictionary<string, double> noCladBridgingLengths;
+            private Dictionary<string, double> metalCladBridgingValues;
+            private Dictionary<string, double> noCladBridgingValues;
             private double exposedGroundFloorBriging;
 
-            public ZoneBridging(Zone zone)
+            private Dictionary<string, string> metalCladAttributes = new Dictionary<string, string>
+            {
+                { "Wall-External Floor", "L5JunctWallExtFlrMC" },
+                { "Wall-Internal Floor", "L5JunctWallFlrMC" },
+                { "Roof-Wall", "L5JunctRoofWallMC" },
+                { "Wall-Wall", "L5JunctWallWallMC" },
+                { "Wall-Ground Floor", "JunctWallGrndMC" },
+                { "Lintel above window or door", "L5JunctLintelMC" },
+                { "Sill below window", "L5JunctSillMC" },
+                { "Jamb at window or door", "L5JunctJambMC" },
+            };
+
+            private Dictionary<string, string> noCladAttributes = new Dictionary<string, string>
+            {
+                { "Wall-External Floor", "L5JunctWallExtFlr" },
+                { "Wall-Internal Floor", "L5JunctWallFlr" },
+                { "Roof-Wall", "L5JunctRoofWall" },
+                { "Wall-Wall", "L5JunctWallWall" },
+                { "Wall-Ground Floor", "L5JunctWallGrnd" },
+                { "Lintel above window or door", "L5JunctLintel" },
+                { "Sill below window", "L5JunctSill" },
+                { "Jamb at window or door", "L5JunctJamb" }
+            };
+
+
+            public ZoneBridging(string title, Zone zone)
             {
                 ZoneBridgingLengths bridgingLengths = zone.ThermalBridgingLengths;
-                zoneName = zone.IdfName;
-                noCladBridging = new Dictionary<string, double>
+                zoneName = title;
+                noCladBridgingLengths = new Dictionary<string, double>
                 {
-                    { "WallExternalFloorLength", bridgingLengths.WallExternalFloorLength },
-                    { "WallInternalFloorLength", bridgingLengths.WallInternalFloorLength },
-                    { "WallRoofLength", bridgingLengths.WallRoofLength },
-                    { "WallWallLength", bridgingLengths.WallWallLength },
-                    { "WallGroundFloorLength", bridgingLengths.WallGroundFloorLength },
-                    { "WallLintelLength", bridgingLengths.WallLintelLength },
-                    { "WallCillLength", bridgingLengths.WallCillLength },
-                    { "WallJambLength", bridgingLengths.WallJambLength }
+                    { "Wall-External Floor", bridgingLengths.WallExternalFloorLength },
+                    { "Wall-Internal Floor", bridgingLengths.WallInternalFloorLength },
+                    { "Roof-Wall", bridgingLengths.WallRoofLength },
+                    { "Wall-Wall", bridgingLengths.WallWallLength },
+                    { "Wall-Ground Floor", bridgingLengths.WallGroundFloorLength },
+                    { "Lintel above window or door", bridgingLengths.WallLintelLength },
+                    { "Sill below window", bridgingLengths.WallCillLength },
+                    { "Jamb at window or door", bridgingLengths.WallJambLength }
                 };
-                metalCladBridging = new Dictionary<string, double>
+                noCladBridgingValues = new Dictionary<string, double>();
+                foreach (KeyValuePair<string, string> kvp in noCladAttributes)
                 {
-                    { "MetalWallExternalFloorLength", bridgingLengths.MetalWallExternalFloorLength },
-                    { "MetalWallInternalFloorLength", bridgingLengths.MetalWallInternalFloorLength },
-                    { "MetalWallRoofLength", bridgingLengths.MetalWallRoofLength },
-                    { "MetalWallWallLength", bridgingLengths.MetalWallWallLength },
-                    { "MetalWallGroundFloorLength", bridgingLengths.MetalWallGroundFloorLength },
-                    { "MetalWallLintelLength", bridgingLengths.MetalWallLintelLength },
-                    { "MetalWallCillLength", bridgingLengths.MetalWallCillLength },
-                    { "MetalWallJambLength", bridgingLengths.MetalWallJambLength },
+                    noCladBridgingValues[kvp.Key] = zone.GetAttributeAsDouble(kvp.Value);
+                }
+
+                metalCladBridgingLengths = new Dictionary<string, double>
+                {
+                    { "Wall-External Floor", bridgingLengths.MetalWallExternalFloorLength },
+                    { "Wall-Internal Floor", bridgingLengths.MetalWallInternalFloorLength },
+                    { "Roof-Wall", bridgingLengths.MetalWallRoofLength },
+                    { "Wall-Wall", bridgingLengths.MetalWallWallLength },
+                    { "Wall-Ground Floor", bridgingLengths.MetalWallGroundFloorLength },
+                    { "Lintel above window or door", bridgingLengths.MetalWallLintelLength },
+                    { "Sill below window", bridgingLengths.MetalWallCillLength },
+                    { "Jamb at window or door", bridgingLengths.MetalWallJambLength },
                 };
+                metalCladBridgingValues = new Dictionary<string, double>();
+                foreach (KeyValuePair<string, string> kvp in metalCladAttributes)
+                {
+                    metalCladBridgingValues[kvp.Key] = zone.GetAttributeAsDouble(kvp.Value);
+                }
+
                 exposedGroundFloorBriging = bridgingLengths.ExposedWallGroundFloorLength;
+            } 
+
+            private string GetGroupReport(string groupTitle, Dictionary<string, double> lengths,
+                Dictionary<string, double> values, bool includeZeroLength)
+            {
+                StringBuilder groupReport = new StringBuilder();
+                groupReport.AppendFormat("  {0}:{1}", groupTitle, Environment.NewLine);
+                bool nonZeroIncluded = false;
+                foreach (KeyValuePair<string, double> kvp in lengths)
+                {
+                    if (includeZeroLength || kvp.Value > 0)
+                    {
+                        string delimitedKey = kvp.Key + ":";
+                        string paddedKey = delimitedKey.PadRight(30);
+                        groupReport.AppendFormat("    - {0} {1,5:0.00}m    {2,6:0.000}W/(m.K){3}", paddedKey, kvp.Value, values[kvp.Key],
+                            Environment.NewLine);
+                        nonZeroIncluded = true;
+                    }
+                }
+                if (!nonZeroIncluded)
+                {
+                    groupReport.AppendLine("    - None");
+                }
+                return groupReport.ToString();
             }
 
-            public string GetReport()
+            public string GetReport(bool includeZeroLength = true)
             {
                 StringBuilder report = new StringBuilder();
                 report.AppendFormat("Zone: {0}{1}", zoneName, Environment.NewLine);
-                report.AppendLine("  Psi values NOT involving metal cladding:");
-                foreach (KeyValuePair<string, double> kvp in noCladBridging)
-                {
-                    report.AppendFormat("    - {0}: {1:0.00}m{2}", kvp.Key, kvp.Value, Environment.NewLine);
-                }
+                string noCladReport =  GetGroupReport( "Psi values NOT involving metal cladding", noCladBridgingLengths, noCladBridgingValues, includeZeroLength);
+                report.Append(noCladReport);
+                report.AppendLine();
 
-                report.AppendLine("");
-                report.AppendLine("  Psi values involving metal cladding:");
-                foreach (KeyValuePair<string, double> kvp in metalCladBridging)
-                {
-                    report.AppendFormat("    - {0}: {1:0.00}m{2}", kvp.Key, kvp.Value, Environment.NewLine);
-                }
+                string metalCladReport = GetGroupReport("Psi values involving metal cladding", metalCladBridgingLengths, metalCladBridgingValues, includeZeroLength);
+                report.Append(metalCladReport);
+                report.AppendLine();
 
-                report.AppendLine("");
-                report.AppendLine("  Exposed ground floor Psi values:");
-                report.AppendFormat("    - ExposedWallGroundFloorLength: {0:0.00}m{1}", exposedGroundFloorBriging,
-                    Environment.NewLine);
+                report.AppendFormat("  Exposed ground floor length:{0,10:0.00}m{1}", exposedGroundFloorBriging, Environment.NewLine);
                 return report.ToString();
             }
         }
